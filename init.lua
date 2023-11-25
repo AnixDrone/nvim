2bbaf3e24dfa487ea0d5a00fbb309ccdf7c125af
AnixDrone <tomislavignjatov@rocketmail.com>
2023-11-25 15:59:32 +0100

    add vim-surround

--- init.lua
+++ init.lua
@@ -75,6 +75,8 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  -- surround text objects
+  'tpope/vim-surround',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
